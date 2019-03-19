ae68dae9cfbf8a3ea52f36ba3e384581085be11e
lisal <lisal@engimusing.com>
2019-03-18 18:44:01 -0600

    Made description box bigger

--- timepiece/entries/models.py
+++ timepiece/entries/models.py
@@ -311,7 +311,7 @@ class ProjectHours(models.Model):
 class ToDo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE,)
     priority = models.IntegerField()
-    description = models.CharField(max_length=500, blank=False)
+    description = models.TextField(max_length=500, blank=False, validators=[validators.MinValueValidator(500, 'must be less than 500 characters')])
     completed = models.BooleanField(blank=False, default=False)
 
     class Meta:
