89b35e63d7045ca1686eb21f17c7f1f5855e1844
Megan <megan.miller@smoldev.dev>
2021-08-02 15:10:59 -0600

    Remove Redirect folder.

--- mrp/settings/local.py
+++ mrp/settings/local.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'timepiece.apps.TimepieceConfig',
     'timepiece.manager',
     'timepiece.entries',
-    'redirect.apps.RedirectConfig',
+    # 'redirect.apps.RedirectConfig',
     'import_export',
     'storages',
 ]
@@ -69,7 +69,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     #'django.contrib.redirects.middleware.RedirectFallbackMiddleware',
-    'mrp.middleware.middleware.RedirectFallbackMiddleware',
+    # 'mrp.middleware.middleware.RedirectFallbackMiddleware',
 ]
 
 
--- redirect/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class RedirectConfig(AppConfig):
-    name = 'redirect'
-
-    def ready(self):
-        import redirect.signals
